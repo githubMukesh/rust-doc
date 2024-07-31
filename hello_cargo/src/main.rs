420a025d406e531bb112c735b16fb2f34fd4fc19
Mukesh Ranjan <mukeshranjan@Mukeshs-MacBook-Pro.local>
2024-07-31 11:25:42 +0530

    Add variables and shadowing

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+
+    let a = 5;
+
+    let a = 5;
+    
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    {
+       let a = a * 9;
+
+       println!("I am printing from curly braces {}", a);
+    }
+
+    println!("The value of a {} and seconds {}", a, THREE_HOURS_IN_SECONDS)
 }
