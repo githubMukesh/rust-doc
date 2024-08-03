7043ec551103365ab62281042502ccf76c4b363f
Mukesh Ranjan <mukeshranjan@Mukeshs-MacBook-Pro.local>
2024-08-03 12:04:45 +0530

    Tupes and Array

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,17 +1,54 @@
+use std::{io::stdin, string};
+
 fn main() {
     println!("Hello, world!");
 
-    let a = 5;
+    // Shadowing
+    // let a = 5;
 
-    let a = 5;
+    // let a = 5;
+
+    // let guess: String = "abcs".parse().expect("Not a number!");
     
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
-    {
-       let a = a * 9;
+    // {
+    //    let a = a * 9;
 
-       println!("I am printing from curly braces {}", a);
-    }
+    //    println!("I am printing from curly braces {}", a);
+    // }
 
-    println!("The value of a {} and seconds {}", a, THREE_HOURS_IN_SECONDS)
+    // println!("The value of a {} and seconds {} and guess {}", a, THREE_HOURS_IN_SECONDS, guess);
+
+    /* ----------------------------------------------Tuples and Array---------------------------- */
+
+    println!("----------------------------------------------Tuples and Array----------------------------");
+ 
+    // let tup = ("Thomas", 18, 99.99);
+
+    // let (name, age, wealth) = tup;
+
+    // println!("Name {} Age {} Wealth {}", name, age, wealth);
+
+    println!("----------------------Generating error---------------------");
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+      stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+
 }
